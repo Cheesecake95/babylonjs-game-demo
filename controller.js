be9125967af76823d862bd930997662c83acdb01
damo <wushengyu1104@qq.com>
2022-06-30 23:11:06 +0900

    devide into parts

--- controller.js
+++ controller.js
@@ -1,3 +1,67 @@
-function keysDown() {
+function keysDown(scene) {
+  this.command = {
+    frameTime: 0,
+    moveForwardKeyDown: false,
+    moveBackwardKeyDown: false,
+    moveLeftKeyDown: false,
+    moveRightKeyDown: false,
+    jumpKeyDown: false,
+    cameraAlpha: 0,
+    cameraBeta: 0
+  }
 
+  scene.onKeyboardObservable.add(kbInfo => {
+    switch (kbInfo.type) {
+      case BABYLON.KeyboardEventTypes.KEYDOWN:
+        switch (kbInfo.event.key) {
+          case 'w':
+          case 'W':
+            this.command.moveForwardKeyDown = true;
+            break;
+          case 'a':
+          case 'A':
+            this.command.moveLeftKeyDown = true;
+            break;
+          case 's':
+          case 'S':
+            this.command.moveBackwardKeyDown = true;
+            break;
+          case 'd':
+          case 'D':
+            this.command.moveRightKeyDown = true;
+            break;
+          case ' ':
+            this.command.jumpKeyDown = true;
+            break;
+        }
+        break;
+      case BABYLON.KeyboardEventTypes.KEYUP:
+        switch (kbInfo.event.key) {
+          case 'w':
+          case 'W':
+            this.command.moveForwardKeyDown = false;
+            break;
+          case 'a':
+          case 'A':
+            this.command.moveLeftKeyDown = false;
+            break;
+          case 's':
+          case 'S':
+            this.command.moveBackwardKeyDown = false;
+            break;
+          case 'd':
+          case 'D':
+            this.command.moveRightKeyDown = false;
+            break;
+          case ' ':
+            this.command.jumpKeyDown = false;
+            break;
+        }
+        break;
+    }
+  });
+
+  return this;
 }
+
+export default keysDown;
--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { Player } from "./player.js";
+import Player from "./player.js";
 
 const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas, true);
@@ -61,129 +61,17 @@ const createScene = function () {
   slide.checkCollisions = true;
   obstacles.push(slide);
 
-  const command = {
-    frameTime: 0,
-    moveForwardKeyDown: false,
-    moveBackwardKeyDown: false,
-    moveLeftKeyDown: false,
-    moveRightKeyDown: false,
-    jumpKeyDown: false,
-    cameraAlpha: 0,
-    cameraBeta: 0
-  }
-
-  scene.onKeyboardObservable.add(kbInfo => {
-    switch (kbInfo.type) {
-      case BABYLON.KeyboardEventTypes.KEYDOWN:
-        switch (kbInfo.event.key) {
-          case 'w':
-          case 'W':
-            command.moveForwardKeyDown = true;
-            break;
-          case 'a':
-          case 'A':
-            command.moveLeftKeyDown = true;
-            break;
-          case 's':
-          case 'S':
-            command.moveBackwardKeyDown = true;
-            break;
-          case 'd':
-          case 'D':
-            command.moveRightKeyDown = true;
-            break;
-          case ' ':
-            command.jumpKeyDown = true;
-            break;
-        }
-        break;
-      case BABYLON.KeyboardEventTypes.KEYUP:
-        switch (kbInfo.event.key) {
-          case 'w':
-          case 'W':
-            command.moveForwardKeyDown = false;
-            break;
-          case 'a':
-          case 'A':
-            command.moveLeftKeyDown = false;
-            break;
-          case 's':
-          case 'S':
-            command.moveBackwardKeyDown = false;
-            break;
-          case 'd':
-          case 'D':
-            command.moveRightKeyDown = false;
-            break;
-          case ' ':
-            command.jumpKeyDown = false;
-            break;
-        }
-        break;
-    }
-  });
-
   scene.registerBeforeRender(() => {
     // Update camera target
     const cameraTargetMeshOffsetPosition = cameraTargetMesh.absolutePosition.add(new BABYLON.Vector3(0, 1, 0));
     camera.target.copyFrom(cameraTargetMeshOffsetPosition);
 
     // Player move
-    command.frameTime = Date.now();
-    command.cameraAlpha = camera.alpha;
-    command.cameraBeta = camera.beta;
-    move(command);
+    _player._keyDown.command.frameTime = Date.now();
+    _player._keyDown.command.cameraAlpha = camera.alpha;
+    _player._keyDown.command.cameraBeta = camera.beta;
+    _player.move(_player._keyDown.command);
   });
-
-  let prevFrameTime;
-  const direction = new BABYLON.Vector3();
-  const velocity = new BABYLON.Vector3();
-  // @ts-ignore
-  const ray = new BABYLON.Ray();
-  const rayHelper = new BABYLON.RayHelper(ray);
-  rayHelper.attachToMesh(_player.mesh, new BABYLON.Vector3(0, -0.995, 0), new BABYLON.Vector3(0, -1, 0), 0.1);
-  rayHelper.show(scene, new BABYLON.Color3(1, 0, 0));
-  let onObject = false;
-  const jump = () => {
-    velocity.y = 0.15;
-    onObject = false;
-  }
-  const move = (command) => {
-    if (prevFrameTime === undefined) {
-      prevFrameTime = command.frameTime;
-      return;
-    }
-
-    const delta = command.frameTime - prevFrameTime;
-
-    // Raycast Method 1
-    const pick = scene.pickWithRay(ray);
-    if (pick) onObject = pick.hit;
-
-    const viewAngleY = 2 * Math.PI - command.cameraAlpha;
-    _player.mesh.rotationQuaternion = BABYLON.Quaternion.FromEulerAngles(0, viewAngleY, 0);
-
-    direction.x = -(Number(command.moveForwardKeyDown) - Number(command.moveBackwardKeyDown));
-    direction.z = Number(command.moveRightKeyDown) - Number(command.moveLeftKeyDown);
-    direction.normalize();
-
-    velocity.x = 0;
-    velocity.z = 0;
-    if (command.moveRightKeyDown || command.moveLeftKeyDown) velocity.z = direction.z * delta / 300;
-    if (command.moveForwardKeyDown || command.moveBackwardKeyDown) velocity.x = direction.x * delta / 300;
-
-    // velocity.y = command.startVelocityY;
-    velocity.y -= delta / 3000;
-    if (onObject) velocity.y = Math.max(0, velocity.y);
-
-    if (command.jumpKeyDown && onObject) jump();
-
-    const rotationAxis = BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, viewAngleY);
-    const rotatedVelocity = BABYLON.Vector3.TransformCoordinates(velocity, rotationAxis);
-    _player.mesh.moveWithCollisions(rotatedVelocity);
-
-    prevFrameTime = command.frameTime;
-  }
 
   const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
 
