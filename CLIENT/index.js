fc05062edbdf4e07684512e069f8467b6219075b
damo <wushengyu1104@qq.com>
2022-07-11 08:00:17 +0900

    1

--- CLIENT/index.js
+++ CLIENT/index.js
@@ -1,5 +1,11 @@
-var socket = new WebSocket("wss://nys2vz12v6.execute-api.ap-northeast-1.amazonaws.com/production/");
+const socket = new WebSocket("wwss://65loa4x8k0.execute-api.ap-northeast-1.amazonaws.com/production");
 
+//连接建立时触发
 socket.onopen = function () {
+  //调用LAMBDA的$connect发送“已进入房间”
   socket.send(`"action":"$connect"`);
 }
+//收到消息时触发
+socket.onmessage = function (event) {
+  console.log(event.body);
+};
