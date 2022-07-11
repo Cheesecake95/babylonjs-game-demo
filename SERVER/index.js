bbfbdd362dd82f51268e9f09b822208677d2ce07
damo <wushengyu1104@qq.com>
2022-07-11 22:35:45 +0900

    1

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,36 +1,9 @@
 const AWS = require('aws-sdk');
 const ENDPOINT = 'nys2vz12v6.execute-api.ap-northeast-1.amazonaws.com/production/';
-const client = new AWS.ApiGatewayManagementApi({ endpoint: ENDPOINT });
-const docClient = new AWS.DynamoDB.DocumentClient();
+const events = require('events');
+const emitter = new events.EventEmitter();
 
-//所有连接中的用户的name
-let names = {};
-
-exports.handler = async (event) => {
-
-  if (event.requestContext) {
-    // const connenctionId = event.requestContext.connenctionId;
-    const routeKey = event.requestContext.routeKey;
-
-    let body = {};
-    try {
-      if (event.body) {
-        body = JSON.parse(event.body);
-      }
-    } catch (e) { }
-
-    switch (routeKey) {
-      case '$connect':
-        console.log('welcome');
-        break;
-
-      default:
-      // code
-    }
-  }
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
-}
+emitter.on('joinGame', function () {
+  console.log('join game 触发');
+});
+emitter.emit('joinGame');
