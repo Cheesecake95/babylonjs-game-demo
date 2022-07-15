587b3d1967851ba65d5fb059b46559069bf6f2cb
Go <wushnengyu1104@gmail.com>
2022-07-15 16:47:51 +0900

    1

--- CLIENT/index.js
+++ CLIENT/index.js
@@ -4,16 +4,7 @@ import Stairs from "./assets/stairs.js";
 
 const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas, true);
-// const socket = new WebSocket("wss://65loa4x8k0.execute-api.ap-northeast-1.amazonaws.com/production");
-// //连接建立时触发
-// socket.onopen = function () {
-//   //调用LAMBDA的$connect发送“已进入房间”
-//   socket.send(`"action":"$connect"`);
-// }
-// //收到消息时触发
-// socket.onmessage = function (event) {
-//   console.log(event.data);
-// };
+
 const createScene = function () {
   const scene = new BABYLON.Scene(engine);
   const camera = new BABYLON.ArcRotateCamera('camera', 0, Math.PI / 2, 5, new BABYLON.Vector3(), scene);
